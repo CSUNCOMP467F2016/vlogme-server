216b6cda7b8cf49aa1b5a03291edee7c32697a20
Valera <valery.maniuk.55@my.csun.edu>
2016-11-22 22:48:46 -0800

    minor changes

--- endpoint/views.py
+++ endpoint/views.py
@@ -5,5 +5,6 @@ from    .models import VideoResponse
 
 
 def get_videos_by_user(request, username):
-    video_objects =  VideoResponse.objects.filter(author=username)    print(video_objects)
+    video_objects =  VideoResponse.objects.filter(author=username)
+    # print(video_objects)
     return JsonResponse(serializers.serialize('json', video_objects), safe=False)
