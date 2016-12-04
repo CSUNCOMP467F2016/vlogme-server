344f8ebc444ec871ed973752d77b92a73e28a65c
Jordan Yelloz <jordan.yelloz@my.csun.edu>
2016-12-03 21:22:09 -0800

    endpoint.views: added some pagination utilities
    These help extracting limit/offset on requests using GET parameters.

--- endpoint/views.py
+++ endpoint/views.py
@@ -3,6 +3,39 @@ from django.core import serializers
 
 from .models import VideoResponse
 
+_OFFSET_DEFAULT = 0
+_OFFSET_DEFAULT_S = str(_OFFSET_DEFAULT)
+
+_SLICE_SIZE_MAX = 1000
+_SLICE_SIZE_DEFAULT = 10
+_SLICE_SIZE_DEFAULT_S = str(_SLICE_SIZE_DEFAULT)
+
+
+def _clamp(n, a, b):
+    low, high = sorted([a, b])
+    return max(low, min(high, n))
+
+
+def _get_pagination(request, prefix=''):
+
+    offset_param = '{}offset'.format(prefix)
+    limit_param = '{}limit'.format(prefix)
+
+    offset_value = request.GET.get(offset_param, _OFFSET_DEFAULT_S)
+    limit_value = request.GET.get(limit_param, _SLICE_SIZE_DEFAULT_S)
+
+    offset = max(int(offset_value), _OFFSET_DEFAULT)
+    limit = _clamp(int(limit_value), 1, _SLICE_SIZE_MAX)
+
+    return offset, limit
+
+
+def _get_pagination_range(request, prefix=''):
+
+    offset, limit = _get_pagination(request, prefix)
+
+    return offset, offset + limit
+
 
 def get_videos_by_user(request, username):
 
