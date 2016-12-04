d205abe8399e8093cda4f48f3f6d867021741627
Jordan Yelloz <jordan.yelloz@my.csun.edu>
2016-12-03 18:02:13 -0800

    endpoint: unescaped JSON output of views.
    Also, made pep-8 compliant.

--- endpoint/views.py
+++ endpoint/views.py
@@ -1,19 +1,25 @@
-from django.http import JsonResponse
+from django.http import HttpResponse
 from django.core import serializers
-from django.shortcuts import render
-from    .models import VideoResponse
+
+from .models import VideoResponse
 
 
 def get_videos_by_user(request, username):
-    video_objects =  VideoResponse.objects.filter(author=username)
-    # print(video_objects)
-    return JsonResponse(serializers.serialize('json', video_objects), safe=False)
+
+    video_objects = VideoResponse.objects.filter(author=username)
+
+    return HttpResponse(
+        serializers.serialize('json', video_objects),
+        content_type='application/json',
+    )
+
 
 def get_responses_to_video(request, video_id):
 
-    topic_video =  VideoResponse.objects.get(id=video_id)
-
+    topic_video = VideoResponse.objects.get(id=video_id)
     responses = VideoResponse.objects.filter(response_to=topic_video)
 
-    return JsonResponse(serializers.serialize('json', responses), safe=False)
-
+    return HttpResponse(
+        serializers.serialize('json', responses),
+        content_type='application/json',
+    )
